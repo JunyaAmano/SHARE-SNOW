012d48d599ddfcbe0d5e46005c26ceb7748321de
EC2 Default User <ec2-user@ip-172-31-5-20.ap-northeast-1.compute.internal>
2020-12-16 19:43:59 +0000

    [Finish]user_post_comment_Ajax

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -5,11 +5,12 @@ class EventUsersController < ApplicationController
     @id_name = "#event-application-link-#{@event.id}"
     applicant = current_user.event_users.new(event_id: @event.id)
     applicant.save
+    @event.create_notification_apply!(current_user)
   end
 
   def destroy
     @event = Event.find(params[:event_id])
-    @id_name = "#event-application-link-#{@event.id}"    
+    @id_name = "#event-application-link-#{@event.id}"
     applicant = current_user.event_users.find_by(event_id: @event.id)
     applicant.destroy
   end

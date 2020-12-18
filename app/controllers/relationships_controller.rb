f983e5f2c9d2b6d9a4f662e84873772416585d63
EC2 Default User <ec2-user@ip-172-31-5-20.ap-northeast-1.compute.internal>
2020-12-18 13:59:15 +0000

    [Finish]follow_Ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,14 @@ class RelationshipsController < ApplicationController
 
   def create
     @user = User.find(params[:followed_id])
-    @id_name = "#follow-link-#{@user.id}%>"
+    @id_name = "#follow-link-#{@user.id}"
     current_user.follow(@user)
     @user.create_notification_follow!(current_user)
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
-    @id_name = "#follow-link-#{@user.id}%>"
+    @id_name = "#follow-link-#{@user.id}"
     current_user.unfollow(@user)
   end
 

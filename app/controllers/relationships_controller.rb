7609132db4b9038e5ad611c643fd4606980f0a15
EC2 Default User <ec2-user@ip-172-31-5-20.ap-northeast-1.compute.internal>
2020-12-18 16:16:33 +0000

    [Fix]model

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,6 +3,7 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     @id_name = "#follow-link-#{@user.id}"
+    @id_name2 = ".follower-count-#{@user.id}"
     current_user.follow(@user)
     @user.create_notification_follow!(current_user)
   end
@@ -10,6 +11,7 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find(params[:id]).followed
     @id_name = "#follow-link-#{@user.id}"
+    @id_name2 = ".follower-count-#{@user.id}"
     current_user.unfollow(@user)
   end
 
